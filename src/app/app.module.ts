a6fd0ab6cfb4ffe540ea57b28764506916ecf65e
RichardMtz18 <utm20040085@utma.edu.mx>
2022-08-04 13:50:06 -0500

    commit proyecto preterminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrudComponent } from './pages/crud/crud.component';
+import { RegistrarEmpresaComponent } from './pages/crud/registrar-empresa/registrar-empresa.component';
+import { ActualizarEmpresaComponent } from './pages/crud/actualizar-empresa/actualizar-empresa.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrudComponent,
+    RegistrarEmpresaComponent,
+    ActualizarEmpresaComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
